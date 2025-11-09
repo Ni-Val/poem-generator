416506bee4f73820f39512ad0061086efa4d87aa
Ni-Val <nivale2003@gmail.com>
2025-11-09 18:48:32 -0500

    improved UX
    +loading animation

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<span class="loader"></span>`;
+
   console.log("generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Prompt: ${context}`);
